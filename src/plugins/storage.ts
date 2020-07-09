99b895725c1446f03d89f1022cec0cc1be451483
Dmitriy Nevezhin <44970286+jimhucksly@users.noreply.github.com>
2020-07-09 20:01:32 +0300

    upd class storage

--- src/plugins/storage.ts
+++ src/plugins/storage.ts
@@ -1,13 +1,19 @@
-const fs = require('fs')
-const path = require('path')
+import fs from "fs"
+import path from 'path'
 
-class Storage  {
+const debug = false
+
+class Storage {
   public isPathExists(_path: string): Promise<object> {
-    return new Promise((resolve, reject) => {
-      fs.access(_path, (err: Error) => {
+    debug && console.log('isPathExists: call!')
+    return new Promise(async (resolve, reject) => {
+      debug && console.log('isPathExists: body of promise')
+      fs.stat(_path, (err, stat) => {
         if(err) {
+          debug && console.log('isPathExists: rejected!')
           return reject(err)
         } else {
+          debug && console.log('isPathExists: resolved!')
           return resolve()
         }
       })
@@ -15,14 +21,21 @@ class Storage  {
   }
 
   public isFileExists(_path: string, _file?: string): Promise<object> {
-    return new Promise((resolve, reject) => {
+    debug && console.log('call isFileExists')
+    return new Promise(async (resolve, reject) => {
       let filePath = _path
-      if(_file !== undefined) filePath = path.resolve(_path, _file)
-      fs.access(filePath, (err: Error) => {
+      if(_file !== undefined) {
+        filePath = path.resolve(_path, _file)
+      }
+      debug && console.log('call fs.stat')
+      fs.stat(filePath, (err, stat) => {
+        debug && console.log('fs.stat callback exec!!!!!')
         if(err) {
-          return reject(err)
+          debug && console.log('isFileExists: rejected!')
+          return reject(null)
         } else {
-          return resolve()
+          debug && console.log('isFileExists: resolved!')
+          return resolve({})
         }
       })
     })
@@ -33,13 +46,13 @@ class Storage  {
       const fullPath = path.resolve(_path, fileName)
       this.isFileExists(fullPath)
         .then(() => {
-          let targetJson = fs.readFileSync(fullPath, 'utf8')
+          let targetJson: any = fs.readFileSync(fullPath, 'utf8')
           try {
             targetJson = JSON.parse(targetJson)
           } catch (err) {
-            return reject(err)
+            targetJson = {}
           }
-          let data = Object.assign({}, targetJson, json)
+          const data = { ...targetJson, ...json }
           this.set(_path, fileName, data)
         })
         .catch(() => {
@@ -49,18 +62,18 @@ class Storage  {
   }
 
   public set(_path: string, fileName: string, json: object): Promise<object> {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       let data
       try {
-        if(json) {
-          data = JSON.stringify(json)
-        } else {
-          data = null
-        }
+        data = json ? JSON.stringify(json) : '{}'
       } catch (err) {
-        return reject(err)
+        data = '{}'
       }
       const fullPath = path.resolve(_path, fileName)
+      const sResponse = await this.isFileExists(fullPath)
+      if(!sResponse) {
+        reject(null)
+      }
       try {
         fs.writeFileSync(fullPath, data)
         resolve()
@@ -71,24 +84,29 @@ class Storage  {
   }
 
   public get(_path: string, _file: string, key?: string): Promise<object> {
-    return new Promise((resolve, reject) => {
+    debug && console.log('call storage.get')
+    return new Promise(async (resolve, reject) => {
       let fullPath = _path
       if(_file !== undefined) fullPath = path.resolve(_path, _file)
-      this.isFileExists(fullPath)
-        .then(() => {
-          const data = fs.readFileSync(fullPath, 'utf8')
-          let json
-          try {
-            json = JSON.parse(data)
-          } catch (err) {
-            return reject(err)
-          }
-          if(key && json[key] !== undefined) resolve(json[key])
-          else resolve(json)
-        })
-        .catch((err: any) => {
-          reject(err)
-        })
+      try {
+        debug && console.log('call storage.isFileExists')
+        const sResponse = await this.isFileExists(fullPath)
+        if(!sResponse) {
+          return reject(null)
+        }
+        const data = fs.readFileSync(fullPath, 'utf8')
+        let json
+        try {
+          json = JSON.parse(data)
+        } catch (err) {
+          json = {}
+        }
+        if(key && json[key] !== undefined) {
+          resolve(json[key])
+        } else resolve(json)
+      } catch(err) {
+        return reject(null)
+      }
     })
   }
 }
